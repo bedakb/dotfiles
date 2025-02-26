424f9a99bb5622a49666a8c84252ea33706f03ac
bedakb <bedakbelmin@gmail.com>
2025-02-26 17:15:52 +0100

    vim and tmux integration

--- init.lua
+++ init.lua
@@ -617,4 +617,19 @@ require('lazy').setup {
       })
     end,
   },
+  { -- vim and tmux integration
+    'christoomey/vim-tmux-navigator',
+    cmd = {
+      'TmuxNavigateLeft',
+      'TmuxNavigateDown',
+      'TmuxNavigateUp',
+      'TmuxNavigateRight',
+    },
+    keys = {
+      { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
+      { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
+      { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
+      { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
+    },
+  },
 }
